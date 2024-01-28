43a9119e0040fe1c79a220a8e59fe8a8e76c5be2
Arthur <arthurflash36@gmail.com>
2024-01-28 17:16:36 -0300

    resolução

--- JavaScript/Aula_1.js
+++ JavaScript/Aula_1.js
@@ -1,14 +1,16 @@
-let var1 = 10;
-let var2 = 10;
-
-console.log(var1 / var2);
-
 // Desafio aula 1
 
 // Faça um prgrama para calcular o valor de uma viagem.
 // Você tera 3 variaveis. sendo elas: 
 // 1- preço do combustivel
 // 2- gasto médio de combustivel do carro por KM
-// 3- Distância em KM da viagem;
+// 3- Distância em KM da viagem
 
 // imprima o valor que será gasto de combustivel para realizar esta viagem.
+
+let var1 = 3.50;
+let var2 = 0.5;
+let var3 = 50;
+let resultado = (var2 * var3) * var1;
+
+console.log(resultado);
