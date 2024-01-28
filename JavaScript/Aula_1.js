66535266cdaa17211362c97df30c4c71bedc08c7
Arthur <arthurflash36@gmail.com>
2024-01-28 16:52:10 -0300

    Desafio aula 1

--- JavaScript/Aula_1.js
+++ JavaScript/Aula_1.js
@@ -1,3 +1,14 @@
+let var1 = 10;
+let var2 = 10;
 
+console.log(var1 / var2);
 
-console.log("imprimir 1");
+// Desafio aula 1
+
+// Faça um prgrama para calcular o valor de uma viagem.
+// Você tera 3 variaveis. sendo elas: 
+// 1- preço do combustivel
+// 2- gasto médio de combustivel do carro por KM
+// 3- Distância em KM da viagem;
+
+// imprima o valor que será gasto de combustivel para realizar esta viagem.
