3136932d83018df06dc09296ad79a49b3e3a689f
Arthur <arthurflash36@gmail.com>
2024-02-06 21:38:26 -0300

    exercicio 2

--- JavaScript/Aula_2.js
+++ JavaScript/Aula_2.js
@@ -2,17 +2,23 @@
 // uma condição (condicional) sempre retorna um boolean
 // = "atribuição", == "igualdade", === "ele é igualdade idenpendete do tipo"
 
-const numero = 0;
-const numeroDivisivelPor5 = (numero % 5) === 0;
+// Desafio aula 2
 
-if (numero === 0) {
-    console.log('o numero é invalido');
-} else if (numeroDivisivelPor5) {
-    console.log();
-}
+// Faça um prgrama para calcular o valor de uma viagem.
+// Você tera 5 variaveis. sendo elas: 
+// 1- preço do etanol
+// 2-preço da gasolina
+// 3- o tipo de combustivel do carro
+// 4- gasto medio de combustivel do carro por km;
+// 5- distancia em KM da viagem;
 
-if (numeroDivisivelPor5) {
-    console.log('sim');
-} else {
-    console.log('não');
-}
+// imprima o valor que será gasto de combustivel para realizar esta viagem;
+
+
+let PrecoEtanol = 5.00;
+let PrecoGasolina = 4.50;
+let TipoCombustivel = (PrecoEtanol = 1) (PrecoGasolina = 2);
+let DistanciaEmKM = 20;
+let GastoMedioPorKMEtanol = PrecoEtanol + DistanciaEmKM;
+let GastoMedioPorGasolina = PrecoGasolina + DistanciaEmKM;
+
