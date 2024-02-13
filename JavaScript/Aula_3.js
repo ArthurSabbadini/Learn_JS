454eb212b9b0feb7fa06c7d551e141d5b734679b
Arthur <arthurflash36@gmail.com>
2024-02-13 13:43:23 -0300

    resolução exercicio 3

--- JavaScript/Aula_3.js
+++ JavaScript/Aula_3.js
@@ -1,4 +1,4 @@
-/*  1) faça um algoritimp que dado as 3 notas tiradas por um aluno em um semestre da faculdade
+/*  1) faça um algoritimo que dado as 3 notas tiradas por um aluno em um semestre da faculdade
     calcule e imprima sua media e sua classificação conforme sua media a baixo
 
     media = (nota 1 + nota 2 + nota 3) / 3;
@@ -9,3 +9,17 @@
     -media acima de 7, passou de semestre;
 */  
 
+
+const nota1 = 7
+const nota2 = 5
+const nota3 = 6
+
+media = (nota1 + nota2 + nota3) / 3;
+
+if (media > 7) {
+    console.log('você foi aprovado e sua media é ' + media.toFixed(1));
+} else if (media > 5 && media < 7) {
+    console.log('Você precisa de recuperação e sua media é ' + media.toFixed(1));
+}else {
+    console.log('Você foi reprovado e sua media é ' + media.toFixed(1));
+}
