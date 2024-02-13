3caf467179060751fdd03c0e37a070d00f378613
Arthur <arthurflash36@gmail.com>
2024-02-13 13:22:09 -0300

    exercicio 3

---
+++ JavaScript/Aula_3.js
@@ -0,0 +1,11 @@
+/*  1) faça um algoritimp que dado as 3 notas tiradas por um aluno em um semestre da faculdade
+    calcule e imprima sua media e sua classificação conforme sua media a baixo
+
+    media = (nota 1 + nota 2 + nota 3) / 3;
+
+    Classificação:
+    -media menor que 5, reprovado;
+    -media entre 5 e 7, recuperação;
+    -media acima de 7, passou de semestre;
+*/  
+
