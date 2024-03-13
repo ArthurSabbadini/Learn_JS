5b2bba5c349bd73143ce8ae294b32968ace6f247
Arthur-Sabbadini <Arthur-Sabbadini>
2024-03-13 17:20:19 -0300

    comentario

--- JavaScript/personagemMorreu.js
+++ JavaScript/personagemMorreu.js
@@ -4,6 +4,8 @@ Faça uma função que receba 2 parâmetros, 'dano' e 'saude'. A função deve r
 o dano for matar o personagem (ou seja, deixar sua saúde menor ou igual a zero) e '0' caso contrário.
 */
 
+//fazendo ...
+
 function personagemMorreu(dano, saude) {
     if (dano >= 1) {
         console.log('Você morreu')
