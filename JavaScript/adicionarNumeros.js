a9ba45450fa69fee8e60db29f6f22813341dead0
Arthur-Sabbadini <Arthur-Sabbadini>
2024-04-29 14:53:23 -0300

    adicionar numeros com functions

---
+++ JavaScript/adicionarNumeros.js
@@ -0,0 +1,27 @@
+/*
+Exercício: Calculadora Simples
+
+Crie uma calculadora simples que execute operações básicas 
+(adição, subtração, multiplicação e divisão).
+*/
+
+function adicionar(num1, num2) {
+    return num1 + num2;
+}
+
+function subtrair(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+
+function dividir(num1, num2) {
+    return num1 / num2;
+}
+
+function resultado(num1, num2) {
+    
+}
+  
